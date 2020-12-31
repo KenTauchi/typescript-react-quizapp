3f1ac7c970d3b1eaf00e1cfb35d5b0470762bdce
Ken Tauchi <kentauchica@gmail.com>
2020-12-30 19:47:07 -0800

    test

--- src/config/server.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const app = express();
-const PORT: string | number = process.env.PORT || 8000;
-
-app.use("*", (req, res) => {
-  res.send("../App.tsx");
-});
-
-app.listen(PORT, () => console.log(`hosting @${PORT}`));
